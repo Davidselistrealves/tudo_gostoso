1afe48e039731201d738549fd20c6f11b783aa2b
David Selistre Alves <davidselistre@gmail.com>
2021-07-20 22:51:19 -0300

    segundo dia do projeto

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tudo_gostoso/style.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -8,12 +10,78 @@ class Home extends StatelessWidget {
     //esqueleto da aplicaçao "Scaffold"
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tudo Gostoso"),
-        //pasta de asstes  esta identada  na pubspec
+        //pasta de assets  esta identada  na pubspec
         leading: Image.asset(
           "assets/images/logo.jpg",
         ),
-        backgroundColor: Colors.deepOrange,
+        title: Text(
+          "Tudo Gostoso",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        //importou a cor do arquivo style da pasta lib
+        backgroundColor: orangeTheme,
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/images/bolo.jpg"),
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 16,
+            ),
+            color: orangeTheme,
+            child: Column(
+              children: [
+                Text(
+                  "Bolo gelado",
+                  style: TextStyle(
+                    fontSize: 34,
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  children: [
+                    //1° Coluna
+                    Column(
+                      children: [
+                        //Icones importados pelo packed fontawesome
+                        FaIcon(
+                          FontAwesomeIcons.solidClock,
+                          color: Colors.white,
+                        ),
+                        //importação de padding pela lupa symmetric para usar vertical ou horizontal
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 5,
+                          ),
+                          child: Text(
+                            "PREPARO",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "40 MIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    //2° Coluna
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
