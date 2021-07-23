6c0497fc92717f7c1112b53b7b21dbe9357991c6
David Selistre Alves <davidselistre@gmail.com>
2021-07-22 22:08:54 -0300

    código dia 22/07

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,87 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:tudo_gostoso/models/recipe.dart';
+import 'package:tudo_gostoso/screens/home/widgets/details.dart';
+import 'package:tudo_gostoso/screens/home/widgets/list_elements.dart';
+import 'package:tudo_gostoso/screens/home/widgets/my_title.dart';
 import 'package:tudo_gostoso/style.dart';
 
 class Home extends StatelessWidget {
+  final recipe = Recipe(
+    preparationTime: 40,
+    income: 12,
+    numberOfFavorites: 332936,
+    numberOfComments: 6847,
+    name: "Bolo gelado",
+    photo: "assets/images/bolo.jpg",
+    preparationModePaste: [
+      "Em uma batedeira, bata as claras em neve acrescentando o açúcar aos poucos e bata por 3 minutos."
+          "Adicione as gemas, o trigo, o suco e continue batendo até formar uma massa homogênea."
+          "Por último, adicione o fermento e bata por mais 40 segundos na menor velocidade da batedeira."
+          "Despeje a massa em uma forma média e untada."
+    ],
+    preparationModeTopping: [
+      " Asse em forno preaquecido em temperatura média de 180 °C por 40 minutos ou até dourar."
+          "Em uma tigela, misture o leite de coco, o leite, o leite condensado e reserve."
+          "Após 40 minutos, retire o bolo do forno e fure toda a sua superfície com um garfo para facilitar a penetração da cobertura."
+          "Com o bolo ainda quente, despeje a cobertura sobre ele e salpique por cima o coco ralado."
+          "Leve a geladeira por 3 horas, depois corte o bolo em quadrados do tamanho que preferir e embrulhe com papel alumínio."
+    ],
+    ingredientsPaste: [
+      "4 ovos",
+      "2 xícaras (chá) de açúcar",
+      "3 xícaras (chá) de farinha de trigo",
+      "1 copo (americano) de suco de laranja",
+      "1 colher (sopa) de fermento em pó",
+    ],
+    ingredientsTopping: [
+      "1 garrafa pequena de leite de coco",
+      "1 garrafa de leite (utilize a mesma garrafa do leite de coco como medida)"
+          "1 lata de leite condensado"
+          "1 pacote de coco ralado sem açúcar",
+    ],
+  );
   @override
-  //metodo que compõe a tela do aplicativo!
-  //tudo que esta dentro da "build" atualiza automaticamente o aplicativo
   Widget build(BuildContext context) {
-    //esqueleto da aplicaçao "Scaffold"
     return Scaffold(
       appBar: AppBar(
-        //pasta de assets  esta identada  na pubspec
         leading: Image.asset(
           "assets/images/logo.jpg",
         ),
         title: Text(
-          "Tudo Gostoso",
+          "Tudo gostoso",
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
-        //importou a cor do arquivo style da pasta lib
         backgroundColor: orangeTheme,
       ),
-      body: Column(
-        children: [
-          Image.asset("assets/images/bolo.jpg"),
-          Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 16,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(recipe.photo),
+            Details(recipe),
+            MyTitle(
+              icon: FontAwesomeIcons.bookOpen,
+              text: "INGREDIENTES",
             ),
-            color: orangeTheme,
-            child: Column(
-              children: [
-                Text(
-                  "Bolo gelado",
-                  style: TextStyle(
-                    fontSize: 34,
-                    color: Colors.white,
-                  ),
-                ),
-                Row(
-                  children: [
-                    //1° Coluna
-                    Column(
-                      children: [
-                        //Icones importados pelo packed fontawesome
-                        FaIcon(
-                          FontAwesomeIcons.solidClock,
-                          color: Colors.white,
-                        ),
-                        //importação de padding pela lupa symmetric para usar vertical ou horizontal
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 5,
-                          ),
-                          child: Text(
-                            "PREPARO",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          "40 MIN",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                      ],
-                    ),
-                    //2° Coluna
-                  ],
-                ),
-              ],
+            ListElements(
+              title: "Massa",
+              listOfElements: recipe.ingredientsPaste,
             ),
-          ),
-        ],
+            ListElements(
+              title: "Cobertura",
+              listOfElements: recipe.ingredientsTopping,
+            ),
+            MyTitle(
+              icon: FontAwesomeIcons.utensilSpoon,
+              text: "MODO DE PREPARO",
+            ),
+            ListElements(
+                listOfElements: recipe.preparationModePaste, title: "Massa"),
+            ListElements(
+              listOfElements: recipe.preparationModeTopping,
+              title: "Cobertura",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
